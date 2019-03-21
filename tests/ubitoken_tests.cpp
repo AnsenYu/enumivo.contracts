a9a1122bd16dc97d9ed109e18400ce28f44f18f0
yichen <yirong.yyr@alibaba-inc.com>
2019-03-21 23:07:18 +0800

    update

--- tests/ubitoken_tests.cpp
+++ tests/ubitoken_tests.cpp
@@ -450,8 +450,11 @@ BOOST_FIXTURE_TEST_CASE( issue_tests, ubitoken_tester ) try {
    produce_blocks(1);
    produce_block( fc::days(30) );
    // carol doesnot trust bob, cannot trustsend
-   BOOST_REQUIRE_EQUAL( wasm_assert_msg( "connection not exist" ), 
+   BOOST_REQUIRE_EQUAL( wasm_assert_msg( "overdrawn balance" ), 
        trustsend( N(alice), N(carol), N(bob), asset::from_string("1.0000 UBI"), "hello" )
+   );
+   BOOST_REQUIRE_EQUAL( wasm_assert_msg( "connection not exist" ), 
+       trustsend( N(alice), N(carol), N(bob), asset::from_string("0.5000 UBI"), "hello" )
    );
 
 } FC_LOG_AND_RETHROW()
